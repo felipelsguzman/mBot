3a68dfe434153133c26fcd566b7e57fc8b2d6d9d
LGsus <jesus.awka@gmail.com>
2017-05-17 17:12:34 -0400

    feat ($mBot) - create main mBot object:
    - koa() - allow to run library as koa middleware

--- src/mBot.js
+++ src/mBot.js
@@ -2,8 +2,96 @@
 
 const EventEmitter = require('events');
 
-module.exports = Object.setPrototypeOf({
-    graphApiVersion: null,
-    fbPageAccessToken: null,
+const messengerApi = {
+
+};
+
+const mBot = Object.setPrototypeOf({
+    // Facebook Graph API version
+    apiUrl: null,
+    // API methods object
+    api: messengerApi,
+    // verify token for webhook usage
+    verifyToken: null,
+    // Facebook Page Access Token
+    pageToken: null,
+    /*
+    *
+    * Handle payload data
+    * @param {array} payload - payload received from facebook messenger
+    *
+    * */
+    handlePayload: async function handlePayload(payload) {
+        console.log('GOT: ', payload);
+    },
+    /*
+     * KOA Middleware
+     * @param {string} apiVersion - facebook graph api version
+     * @param {string} endpoint - endpoint for webhook usage
+     * @param {string} verifyToken - verifyToken for webhook usage
+     * @param {string} pageToken - facebook page access token
+     *
+     * */
+    koa: function koa({
+        apiVersion,
+        endpoint,
+        verifyToken,
+        pageToken
+    }) {
+
+        // set api url
+        this.apiUrl = 'https://graph.facebook.com/' + apiVersion + '/';
+
+        // set default option for endpoint name
+        let url = endpoint === undefined ? '/messenger' : endpoint;
+
+        // check for mandatory access tokens
+        let token = ((pToken, vToken)=>{
+            if (pToken === undefined ||
+                vToken === undefined) {
+                throw new Error('you must set valid values for both pageToken and verifyToken params');
+            } else {
+                // store facebook page access token on main object
+                this.pageToken = pToken;
+                this.verifyToken = vToken;
+
+                return {
+                    page: pToken,
+                    verify: vToken
+                }
+            }
+        })(pageToken, verifyToken);
+
+        // return koa middelware
+        return async function mBotKoa(ctx, next) {
+
+            if (ctx.path === url && ctx.method === 'GET') {
+                //console.log('AKI', this.verifyToken);
+                if (ctx.query['hub.verify_token'] === token.verify) {
+                    ctx.type = 'text/plain; charset=utf-8';
+                    ctx.body = ctx.query['hub.challenge'];
+                    ctx.status  = 200;
+                } else {
+                    ctx.status = 401;
+                }
+            }
+
+            else if (ctx.path === url && ctx.method === 'POST') {
+                if (ctx.request.body.object ==='page') {
+                    let payload = ctx.request.body.entry;
+                    mBot.handlePayload(payload);
+                    ctx.type = 'text/plain; charset=utf-8';
+                    ctx.status = 200;
+                } else {
+                    ctx.status = 400;
+                }
+            }
+
+            await next();
+        }
+    }
+
 }, EventEmitter.prototype);
+
+module.exports = mBot;
 
--- test/mBot.js
+++ test/mBot.js
@@ -3,11 +3,81 @@
 const run = require('ava');
 const test = require('unit.js');
 const mBot = require('../src/mBot');
+const server = require('../index');
+const request = require('supertest');
 
-run('checkProperties', () => {
+// fixtures
+const payload = require('./lib/fixtures/payload');
 
-    test.object(mBot)
-        .hasOwnProperty('graphApiVersion')
-        .hasOwnProperty('fbPageAccessToken');
+const app = server.listen();
 
+run('check messengerAPI object properties', () => {
+
+        test.object(mBot.api);
+
+});
+
+run('GET /messenger - test 401 Unauthorized', async () => {
+    await request(app)
+        .get('/messenger')
+        .query({
+            'hub.verify_token': 'testVerifyToken',
+            'hub.challenge': 9876543210
+        })
+        .expect('Content-type', 'text/plain; charset=utf-8')
+        .expect(401)
+        .then(response => {
+            test.bool(response.unauthorized).isTrue();
+        })
+        .catch(error => {
+            test.fail(error);
+        });
+});
+
+run('GET /messenger - test 200 OK', async () => {
+    await request(app)
+        .get('/messenger')
+        .query({
+            'hub.verify_token': mBot.verifyToken,
+            'hub.challenge': 9876543210
+        })
+        .expect('Content-type', 'text/plain; charset=utf-8')
+        .expect(200)
+        .then(response => {
+            test.bool(response.ok).isTrue();
+        })
+        .catch(error => {
+            test.fail(error);
+        });
+});
+
+run('GET /messenger - test register hook for facebook', async () => {
+    await request(app)
+        .get('/messenger')
+        .query({
+            'hub.verify_token': mBot.verifyToken,
+            'hub.challenge': 9876543210
+        })
+        .expect('Content-type', 'text/plain; charset=utf-8')
+        .expect(200)
+        .then(response => {
+            test.number(Number(response.text)).is(9876543210);
+        })
+        .catch(error => {
+            test.fail(error);
+        });
+});
+
+run('POST /messenger - test webhook', async () => {
+    await request(app)
+        .post('/messenger')
+        .send(payload)
+        .expect('Content-type', 'text/plain; charset=utf-8')
+        .expect(200)
+        .then(response => {
+            test.bool(response.ok).isTrue();
+        })
+        .catch(error => {
+            test.fail(error);
+        });
 });
